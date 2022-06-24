faec1507ad966b32fd71998ebecb1e558fdfd020
Nathan Segers <nathan.segers@howest.be>
2022-06-24 10:57:53 +0200

    bugfix

--- scripts/score.py
+++ scripts/score.py
@@ -12,7 +12,7 @@ def init():
 
     # The AZUREML_MODEL_DIR environment variable indicates
     # a directory containing the model file you registered.
-    model_path = os.path.join(os.environ.get('AZUREML_MODEL_DIR'), 'animal-cnn-test')
+    model_path = os.path.join(os.environ.get('AZUREML_MODEL_DIR'), 'animal-cnn')
 
     model = load_model(model_path)
 
